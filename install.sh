cbd261d936faa142badf89f465c56ecc96b7cbd1
JNSFilipe <jose.filipe@ieee.org>
2022-08-25 16:14:04 +0100

    Added standalone installation script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ln -s ./init.lua ~/.config/nvim/init.lua
+ln -s ./misc.lua ~/.config/nvim/lua/user/misc.lua
+ln -s ./theme.lua ~/.config/nvim/lua/user/theme.lua
+ln -s ./coding.lua ~/.config/nvim/lua/user/coding.lua
+ln -s ./keymaps.lua ~/.config/nvim/lua/user/keymaps.lua
+ln -s ./options.lua ~/.config/nvim/lua/user/options.lua
+ln -s ./plugins.lua ~/.config/nvim/lua/user/plugins.lua
+ln -s ./filetree.lua ~/.config/nvim/lua/user/filetree.lua
+ln -s ./terminal.lua ~/.config/nvim/lua/user/terminal.lua
+ln -s ./whichkey.lua ~/.config/nvim/lua/user/whichkey.lua
+ln -s ./options.lua ~/.config/nvim/lua/user/options.lua
+ln -s ./packerbootstrap.lua ~/.config/nvim/lua/user/packerbootstrap.lua
+
+echo "NVIMIT installes sucessfully!"
+
