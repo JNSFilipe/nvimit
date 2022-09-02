04ed668c1ee78068bfcbc675ad61e89cf13b51d0
JNSFilipe <jose.filipe@ieee.org>
2022-09-02 15:55:50 +0100

    Added basic NeoVide config

--- coding.lua
+++ coding.lua
@@ -296,7 +296,7 @@ tsconfigs.setup {
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
+    disable = { "latex" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = false,  -- Required by spellsitter
   },
   rainbow = {
