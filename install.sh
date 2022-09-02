93fa1b34c43efdf44ba7ae707dd8f196bbf5d085
JNSFilipe <jose.filipe@ieee.org>
2022-09-02 16:02:50 +0100

    Removed duplicated line in installation script

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ ln -s ./plugins.lua ~/.config/nvim/lua/user/plugins.lua
 ln -s ./filetree.lua ~/.config/nvim/lua/user/filetree.lua
 ln -s ./terminal.lua ~/.config/nvim/lua/user/terminal.lua
 ln -s ./whichkey.lua ~/.config/nvim/lua/user/whichkey.lua
-ln -s ./options.lua ~/.config/nvim/lua/user/options.lua
 ln -s ./packerbootstrap.lua ~/.config/nvim/lua/user/packerbootstrap.lua
 
 echo "NVIMIT installed sucessfully!"
